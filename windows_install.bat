104d014038468705de57895b9427138fb200a1b5
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2018-08-15 23:37:22 -0600

    Giving up on Windows Python 26

--- windows_install.bat
+++ windows_install.bat
@@ -1,11 +1,16 @@
-python --version | find "2.6" && goto :getpip
+@echo off
+
+REM echo %PYTHON%
+REM goto :theend
+
+%PYTHON%\python.exe --version 2>&1 | find "2.6" && goto :getpip
 echo %PYTHON%\python.exe -m pip install arrow
 goto :theend
 
 :getpip
 echo Python 2.6 detected
-echo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-echo %PYTHON%\python.exe get-pip.py
+curl https://bootstrap.pypa.io/2.6/get-pip.py -o get-pip.py
+%PYTHON%\python.exe get-pip.py
 echo %PYTHON%\pip.exe install arrow
 
 :theend
